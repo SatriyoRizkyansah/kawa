a888376f95e39ab0ad5d97cd629451a95650c149
Satriyo Rizkyansah <satttrioo@gmail.Com>
2024-12-28 14:59:02 +0700

    redesign login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,50 +2,62 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <div class="container ">
-      <!-- Outer Row -->
-      <div class="row justify-content-center ">
-        <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
-          <div class="card o-hidden border-0 shadow-lg my-5">
-            <div class="card-body p-0">
-              <!-- Nested Row within Card Body -->
-              <div class="row">
-                {{-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> --}}
-                <div class="col-lg-12">
-                  <div class="p-5">
-                    <div class="text-center">
-                      <h1 class="h4 text-gray-900 mb-4">Login page!</h1>
-                    </div>
-                    <form class="user" method="POST" action="{{ route('login')}}" >
-                     @csrf
-                      <div class="form-group">
-                        <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." />
-                      </div>
-                      <div class="form-group">
-                        <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" />
-                      </div>
-                      <div class="form-group">
-                        <div class="custom-control custom-checkbox small">
-                          <input type="checkbox" class="custom-control-input" id="customCheck" name="remember_me"/>
-                          <label class="custom-control-label" for="customCheck">Remember Me</label>
-                        </div>
-                      </div>
-                      
-                      <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
+    <style>
+      body {
+        background-image: url("iamges/bg.png");
+        /* Ganti dengan URL gambar Anda */
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        height: 100vh;
+        position: relative;
+      }
 
-                      <hr />
-                    </form>
-                    {{-- <hr /> --}}
-                    <div class="text-center">
-                      <a class="small" href="{{ route('password.request') }}">Forgot Password?</a>
-                    </div>
-                    {{-- <div class="text-center">
-                      <a class="small" href="register.html">Create an Account!</a>
-                    </div> --}}
-                  </div>
-                </div>
-              </div>
+      /* Menambahkan overlay transparan */
+      body::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 123, 255, 0.812);
+        /* Transparansi 50% */
+        z-index: 1;
+      }
+
+      .container-fluid {
+        position: relative;
+        z-index: 2;
+        /* Agar konten muncul di atas overlay */
+      }
+    </style>
+
+        <div class="container-fluid vh-100 d-flex justify-content-center align-items-center">
+      <div class="card o-hidden border-0 shadow-lg" style="width: 400px">
+        <div class="card-body p-0">
+          <div class="p-4">
+            <div class="text-center d-flex justify-content-between align-items-center mb-3 px-3">
+              <img src="images/sasmita.png" width="50" height="50" alt="" />
+              <h4 class="text-gray-900 font-weight-bold mb-0">LOGIN</h4>
+              <img src="images/logo-unpam.png" width="50" height="50" alt="" />
             </div>
+            <form class="user" method="POST" action="{{ route('login')}}" >
+              @csrf
+              <div class="form-group">
+                <input type="email" name="email" class="form-control form-control-user rounded" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." />
+              </div>
+              <div class="form-group">
+                <input type="password" name="password" class="form-control form-control-user rounded" id="exampleInputPassword" placeholder="Password" />
+              </div>
+              <div class="form-group">
+                <div class="custom-control custom-checkbox small">
+                  <input type="checkbox" class="custom-control-input" id="customCheck" name="remember_me"/>
+                  <label class="custom-control-label" for="customCheck">Remember Me</label>
+                </div>
+                </div>
+              <button type="submit" class="btn btn-primary btn-user btn-block rounded"> Login </button>
+            </form>
           </div>
         </div>
       </div>
